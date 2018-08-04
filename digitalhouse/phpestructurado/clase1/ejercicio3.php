30730b5aa796ec023e68510be18679b1b6353397
Hernan Moran <hernmoran@gmail.com>
2018-08-03 21:46:53 -0300

    mensaje test

--- digitalhouse/phpestructurado/clase1/ejercicio3.php
+++ digitalhouse/phpestructurado/clase1/ejercicio3.php
@@ -59,11 +59,38 @@ $hernan[0][]= "Chau";
 var_dump ($hernan);
 
 
+echo "<br>";
+$dia=3;
+$dia= ["Lunes", "Martes", "Miercoles", "Juesves", "Viernes", "Sabado", "Domingo" ];
+$evaluaciones= 1;
+$evaluaciones= [ "Regular", "Bueno", "Excelente"];
+$porcentaje= [ 10 , 30 , 30];
+$porcentajingreso= 0;
+$nombre="Daniel";
+$apellido="Fuentes";
+$sueldo= 8000;
+$dni="";
+
+if ($evaluaciones[0]){
+    $porcentajecero=$porcentaje[0];
+}
+    
+    elseif ($evaluaciones[1]){
+    $porcentajeingreso=$porcentaje[1];
+}
+
+
+    elseif ($evaluaciones[2]){
+    $porcentajeingreso=$porcentaje[2];
+}
+
+if ($porcentajeingreso == $porcentaje[0]){
+    $sueldo; 
+}
 
 
 
-
-
+ echo "Estimdos(a) ". $nombre. " ". $apellido. ", portador del DNI: ". $dni. ", le informamos que el dia de hoy: ". $dia. ", de acuerdo al nivel de evaluacion obtenido: ". $evaluaciones. ", usted ha recivido un aumento del ". $porcentaje. ", siendo su aumento de: " ;
 
 
 ?>
